1b83e4d96b0006f435c45aaff59a8df2c972d80e
Noah S <Noah3141@gmail.com>
2023-04-16 13:22:31 -0400

    Trying out hash map dictionary

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,18 @@
 mod citation;
 use citation::Citation;
 use citation::CitSourceType;
+use std::collections::HashMap;
 
 // When done testing, build Citation as structs within an Enum{struct BookCit, struct ReviewCit, struct ExpCit}
 // Because a book citation needs different fields in the first place, and the build impl will need to look different for each
 fn main() {
-    // Create a citation
-    let mut citation_01 = Citation::new();
-    citation_01.source_type = CitSourceType::RevArticle;
-    citation_01
-        .authors
-        .push_str("Kupeciec, M., Sandvoll, L. G., & Steckley, N. J. D.");
-    citation_01.year.push_str("2021");
-    citation_01
-        .title
-        .push_str("Effects of Ketogenic Diets on Mental Health");
-    citation_01
-        .journal
-        .push_str("Journal of Metabolic Psychiatry");
 
-    println!("\n\nThe debug output is:\n{:#?}", citation_01);
 
-    let reference_01 = citation_01.build();
-    println!("\nThe build citation is:\n{}\n\n", reference_01);
+    let mut citation_categories = HashMap::new();
+    citation_categories.insert("Psychology", vec!["Perception", "Neuro","Organizational","Clinical"]);
 
-    /*  Error handling by wrapping in Option and 'expecting' non-none */
+    println!("{:?}", citation_categories["Psychology"])
 
-    let some_num: Option<i32> = Some(5); // some_num is of type "Option" (an 'enum'), the variant is set to "Some"-thing, and holds 5
-    // let some_num: Option<i32> = None; // some_num is of type "Option" (an 'enum'), the variant is set to nothing, but *would* hold i32
-
-    let five_plus_unsure_variable: i32 =
-        5 + some_num.expect("Expecting i32 to be set for some_num");
-
-    println!("Total: {}", five_plus_unsure_variable);
-    assert_eq!(10, five_plus_unsure_variable);
-
-    /* Error handling by unwrapping the Option, and dealing with both Some and None possibilities */
-
-    let some_other_num: Option<i32> = Some(5);
-    // let some_other_num: Option<i32> = None;
-
-    // Unwrapping the Option:
-    let the_other_num: i32 = match some_other_num {
-        // Using match on an Option<T> to unpack its possible states
-        None => panic!("Uh oh! some_other_num didn't have something in it."), // If nothing is in it, throw me an error!
-        Some(_num) => some_other_num.unwrap(), // If set to Some, and holding a variable, call it 'num' and return me it!
-    };
-
-    let five_plus_other_unsure_variable: i32 = 5 + the_other_num;
-
-    println!("Total: {}", five_plus_other_unsure_variable);
-    assert_eq!(10, five_plus_other_unsure_variable);
 }
 
 fn break_down_reference_list(_raw_list: String) {
