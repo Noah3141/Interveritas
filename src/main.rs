4ecb0521cc58e22638945caeb99d1d936a1ae1c8
Noah S <Noah3141@gmail.com>
2023-04-16 11:39:14 -0400

    Breaking into multiple files

--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,6 @@
-#[derive(Debug)]
-struct Citation {
-    source_type: CitSourceType,
-    authors: String,
-    year: String,
-    title: String,
-    journal: String,
-    issue_num: String,
-    article_num: String,
-    pp: String,
-    citation_count: String,
-    method: String,
-}
-impl Citation {
-    fn build(mut self) -> String {
-        let mut citation = String::new();
-        let mut parts = vec![
-            self.authors,
-            self.year,
-            self.title,
-            self.journal,
-            self.issue_num,
-            self.article_num,
-            self.pp,
-        ];
-        parts[1] = "(".to_owned() + &parts[1] + ").";
-        for i in parts {
-            citation.push_str((i + " ").as_str());
-        }
-        citation
-    }
-
-    fn new() -> Self {
-        Self {
-            source_type: CitSourceType::Unspecified,
-            authors: String::new(),
-            year: String::new(),
-            title: String::new(),
-            journal: String::new(),
-            issue_num: String::new(),
-            article_num: String::new(),
-            pp: String::new(),
-            citation_count: String::new(),
-            method: String::new(),
-        }
-    }
-}
-
-#[derive(Debug)]
-enum CitSourceType {
-    ExpArticle,
-    RevArticle,
-    Book,
-    Lecture,
-    Unspecified,
-}
-
-enum LocationSpecs {
-    Pages(u16, u16),
-    Doi(u32),
-}
+mod citation;
+use citation::Citation;
+use citation::CitSourceType;
 
 // When done testing, build Citation as structs within an Enum{struct BookCit, struct ReviewCit, struct ExpCit}
 // Because a book citation needs different fields in the first place, and the build impl will need to look different for each
@@ -113,8 +55,10 @@ fn main() {
 
 fn break_down_reference_list(raw_list: String) {
 
+    todo!();
     // Input a copy and pasted listed of references from an article similar to yours, and get
     // 1. Data on year distribution
     // 2. Prevalence of Authors in the literature
     // 3. Prevalence Journal
 }
+
